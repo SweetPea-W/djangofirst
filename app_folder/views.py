56e85645b7567453766b4f1f8102d8b6ca16615d
SweetPea-W <bumpofchicken413@gmail.com>
2020-04-12 12:03:01 +0900

    commitcommit

---
+++ app_folder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+
+class SampleView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app_folder/top_page.html')
+top_page = SampleView.as_view()
+
+from django.shortcuts import render
+
+# Create your views here.
